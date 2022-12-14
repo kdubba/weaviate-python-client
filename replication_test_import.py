ef2706de235c6e8306df2dfa87b4a2033505bb5d
Parker Duckworth <parkerduckworth@gmail.com>
2022-12-14 14:35:15 -0600

    update import script with log file and timestamps

--- replication_test_import.py
+++ replication_test_import.py
@@ -3,6 +3,7 @@ import gc
 import os
 import time
 import json
+import datetime
 import weaviate
 
 
@@ -70,46 +71,34 @@ def prepare_client():
 def import_data(client: weaviate.Client):
     start = time.time()
     c = 0
-    # ids_to_query = []
 
-    with open(SPHERE_DATASET) as jsonl_file:
-        with client.batch as batch:
-            for jsonl in jsonl_file:
-                json_parsed = json.loads(jsonl)
-                batch.add_data_object({
-                        'url':  json_parsed['url'],
-                        'title': json_parsed['title'],
-                        'raw': json_parsed['raw'],
-                        'sha': json_parsed['sha']
-                    },
-                    'Page',
-                    json_parsed['id'],
-                    vector=json_parsed['vector']
-                )
+    with open('import.log', 'w') as f:
+        with open(SPHERE_DATASET) as jsonl_file:
+            with client.batch as batch:
+                for jsonl in jsonl_file:
+                    json_parsed = json.loads(jsonl)
+                    batch.add_data_object({
+                            'url':  json_parsed['url'],
+                            'title': json_parsed['title'],
+                            'raw': json_parsed['raw'],
+                            'sha': json_parsed['sha']
+                        },
+                        'Page',
+                        json_parsed['id'],
+                        vector=json_parsed['vector']
+                    )
 
-                c += 1
+                    c += 1
+                    del json_parsed
 
-                # ids_to_query.append(json_parsed['id'])
-                # if c % 100 == 0:
-                #     validate_replicated_inserts(ids_to_query, c-100, c)
-                
-                del json_parsed
-
-                if (c % (BATCH_SIZE * 1000)) == 0:
-                    print(f'Imported: {c}, batch_size: {client.batch.recommended_num_objects}')
-                    gc.collect()
+                    if (c % (BATCH_SIZE * 1000)) == 0:
+                        print(f'[{datetime.datetime.now(datetime.timezone.utc)}] Imported: {c}, batch_size: {client.batch.recommended_num_objects}')
+                        print(f'[{datetime.datetime.now(datetime.timezone.utc)}] Imported: {c}, batch_size: {client.batch.recommended_num_objects}', file=f)
+                        gc.collect()
 
     end = time.time()
     print('Done in', end - start)
 
-def validate_replicated_inserts(ids_to_query, start, end):
-    for i, obj_id in enumerate(ids_to_query[start:end]):
-            for _, node_name in enumerate(NODE_NAMES):
-                try:
-                    client.data_object.get_by_id(uuid=obj_id, class_name='Page', node_name=node_name)
-                except Exception as e:
-                    print(f'ERROR: object id "{obj_id}" was not found on node "{node_name}"',)
-            
 
 if __name__ == '__main__':
     client = prepare_client()
