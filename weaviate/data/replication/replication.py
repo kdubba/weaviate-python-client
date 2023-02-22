5c4b991281b34bb762449c49678c46494c36b2fd
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2023-02-22 14:45:37 +0100

    Replication: add consistency level to CRUD operations related to single objects and references

--- weaviate/data/replication/replication.py
+++ weaviate/data/replication/replication.py
@@ -1,4 +1,5 @@
 from enum import Enum, EnumMeta, auto
+from typing import Union
 
 
 # MetaEnum and BaseEnum are required to support `in` statements:
@@ -22,3 +23,30 @@ class ConsistencyLevel(str, BaseEnum):
     ALL = auto()
     ONE = auto()
     QUORUM = auto()
+
+
+def name_consistency_level(level: Union[str, ConsistencyLevel]) -> str:
+    """
+    Returns the name of giving consistency level
+
+    Parameters
+    ----------
+    level : str or ConsistencyLevel
+    Consistency level
+
+    Returns
+    -------
+    str
+        The name of the giving consistency level
+
+    Raises
+    ------
+    ValueError
+        If level is not among valid levels
+    """
+    if level not in ConsistencyLevel:
+        raise ValueError(f"invalid ConsistencyLevel: {level}")
+    if isinstance(level, ConsistencyLevel):
+        return level.name
+    else:
+        return level
