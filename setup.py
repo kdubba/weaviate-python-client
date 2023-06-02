23c407f1ebade74ece39d31ed9394154974465fe
krishna <krishna.dubba@jijivi.com>
2023-06-02 21:35:50 +0100

    added version

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        name='weaviate-client',
+        version='3.19.2',  # Specify the version here
+        # other arguments and options
+    )
